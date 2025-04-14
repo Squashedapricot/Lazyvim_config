3c13628eb8f6ff7d6065867f09a900818410adb9
Squashedapricot <shriss999@protonmail.com>
2025-04-14 12:26:24 +0530

    adwaita adapted for oled

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
-  { "Mofiqul/adwaita.nvim" },
+  {
+    "Mofiqul/adwaita.nvim",
+    config = function()
+      vim.g.adwaita_darker = true      -- for darker version
+      vim.g.adwaita_transparent = true -- makes the background transparent
+      vim.cmd('colorscheme adwaita')
+    end
+  },
 
   {
     "LazyVim/LazyVim",
